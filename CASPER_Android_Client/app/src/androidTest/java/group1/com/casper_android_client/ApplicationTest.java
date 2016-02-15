2aca82102fd4e80435b036eedb767d13441820e6
Andreas Fransson <Ankanx@gmail.com>
2016-02-15 15:39:51 +0100

    Initial Commit, Started New Feature for developing the application with test simple Ui.

---
+++ CASPER_Android_Client/app/src/androidTest/java/group1/com/casper_android_client/ApplicationTest.java
@@ -0,0 +1,14 @@
+package group1.com.casper_android_client;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created Monday February 15 - 2016
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
