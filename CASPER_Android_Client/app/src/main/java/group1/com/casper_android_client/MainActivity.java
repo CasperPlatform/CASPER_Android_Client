20403fcc5be4609e6881646b6a8195eceff5b2b5
Andreas Fransson <Ankanx@gmail.com>
2016-02-16 20:09:40 +0100

    Added Icon package.

---
+++ CASPER_Android_Client/app/src/main/java/group1/com/casper_android_client/MainActivity.java
@@ -0,0 +1,19 @@
+package group1.com.casper_android_client;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
